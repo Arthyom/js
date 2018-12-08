ac1c111bc08f1b63a77c2fb835f66791e4c23545
arthyom <a.gonzalezgavina@ugto.mx>
2018-12-07 23:50:14 -0600

    js2

--- evTeclado.js
+++ evTeclado.js
@@ -1,15 +1,32 @@
 // eventos del teclado
-window.onkeydown = tecladoPress;
+window.onkeypress = tecladoPress;
 window.onkeyup = tecladoSolt;
+window.onkeydown = tecladoDown;
+window.onmousemove = ratonMove;
 
 function tecladoPress(info) {
    var car = String.fromCharCode( info.keyCode);
-   document.getElementById('kValDw').innerText  += car;
-   document.getElementById('evSel').innerText = 'Eventos del Teclado: keyDown';
+   document.getElementById('kValPs').innerText  = car;
+   document.getElementById('evSel').innerText = 'Eventos del Teclado: keyPress';
 }
 
 function tecladoSolt(info) {
     var car = String.fromCharCode( info.keyCode);
-    document.getElementById('kValUp').innerText  = texto;
+    document.getElementById('kValUp').innerText  = car;
     document.getElementById('evSel').innerText = 'Eventos del Teclado: keyUp';
  }
+
+ function tecladoDown(info) {
+    var car = String.fromCharCode( info.keyCode);
+    document.getElementById('kValDw').innerText  = car;
+    document.getElementById('evSel').innerText = 'Eventos del Teclado: keyDown';
+ }
+ 
+ function ratonMove(info) {
+    var x = info.clientX;
+    var y = info.clientY;
+
+    document.getElementById('mValMv').innerText  = "x= "+x + ", y= "+y;
+    document.getElementById('evSel').innerText = 'Eventos del Teclado: MouseMove';
+ }
+ 
