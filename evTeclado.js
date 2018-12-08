5d4abe029a9b22f08d0d642ed9f3a3be0607a607
arthyom <a.gonzalezgavina@ugto.mx>
2018-12-08 02:52:52 -0600

    fin de los eventos

--- evTeclado.js
+++ evTeclado.js
@@ -3,6 +3,11 @@ window.onkeypress = tecladoPress;
 window.onkeyup = tecladoSolt;
 window.onkeydown = tecladoDown;
 window.onmousemove = ratonMove;
+window.onmousedown = ratonDown;
+window.onmouseout = ratonOut;
+window.onmouseover = ratonEnter;
+window.onblur = formBlur;
+window.onsubmit = formSub;
 
 function tecladoPress(info) {
    var car = String.fromCharCode( info.keyCode);
@@ -22,6 +27,8 @@ function tecladoSolt(info) {
     document.getElementById('evSel').innerText = 'Eventos del Teclado: keyDown';
  }
  
+///////// raton ////////
+ 
  function ratonMove(info) {
     var x = info.clientX;
     var y = info.clientY;
@@ -30,3 +37,36 @@ function tecladoSolt(info) {
     document.getElementById('evSel').innerText = 'Eventos del Teclado: MouseMove';
  }
  
+
+ function ratonDown(info) {
+    var x = info.clientX;
+    var y = info.clientY;
+
+    document.getElementById('mValPs').innerText  = "x= "+x + ", y= "+y;
+    document.getElementById('evSel').innerText = 'Eventos del Teclado: MousePress';
+ }
+
+
+ function ratonOut(info) {
+    document.getElementById('mValOt').innerText  = "Saliendo del Control";
+    document.getElementById('mValEt').innerText  = "Saliendo del Control";
+    document.getElementById('evSel').innerText = 'Eventos del Teclado: MouseOut';
+ }
+
+
+ function ratonEnter(info) {
+    document.getElementById('mValEt').innerText  = "Entrando en el Control";
+    document.getElementById('mValOt').innerText  = "Entrando en el Control";
+    document.getElementById('evSel').innerText = 'Eventos del Teclado: MouseOver';
+ }
+
+
+ function formBlur(info) {
+    document.getElementById('fValBl').innerText  = "Blur en los controles";
+    document.getElementById('evSel').innerText = 'Eventos del Teclado: Blur';
+ }
+
+ function formSub(info) {
+    document.getElementById('fValSb').innerText  = "Enviando formulario";
+    document.getElementById('evSel').innerText = 'Eventos del Teclado: Submit';
+ }
